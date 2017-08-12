3a69f82efb71dc9849381a00d28447148e6418d2
Sobin <sobinmathew988@gmail.com>
2017-08-12 22:11:13 +0530

    8Task Submitted

---
+++ solution8taskeight.py
@@ -0,0 +1,9 @@
+numberint = 133 
+numberdecimal = 125 
+numberfloat = 1.233 
+numbercomplex = 13.12j
+
+print(numberint,end=' ')
+print(numberdecimal,end=' ')
+print(numberfloat, end=" ")
+print(numbercomplex)
